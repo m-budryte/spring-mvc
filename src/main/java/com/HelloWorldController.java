ff4b98c2cc6ff99b1e2cf27abde60434eb67a311
Masha Budryte <masha.budryte@thoughtworks.com>
2019-10-04 14:48:14 +0100

    using postman to delete request

--- src/main/java/com/HelloWorldController.java
+++ src/main/java/com/HelloWorldController.java
@@ -2,6 +2,7 @@ package com;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,13 +10,35 @@ import java.util.List;
 @RestController
 public class HelloWorldController {
 
-    public static List<List<String>> students = Arrays.asList(
-            Arrays.asList("masha", "sasha", "pasha"), Arrays.asList("xi", "jin", "pin"), Arrays.asList("john", "andrew", "scott"));
+    public static List<ArrayList<String>> students = new ArrayList<ArrayList<String>>();
 
-    @RequestMapping(value = "/getStudents", method = RequestMethod.POST )
+    @RequestMapping(value = "/init")
+    @ResponseBody
+    public void init() {
+        ArrayList<String> h = new ArrayList<String>();
+        h.addAll(Arrays.asList("masha", "sasha", "pasha"));
+        students.add(h);
+
+        h = new ArrayList<String>();
+        h.addAll(Arrays.asList("xi", "jin", "pin"));
+        students.add(h);
+
+        h = new ArrayList<String>();
+        h.addAll(Arrays.asList("john", "andrew", "scott"));
+        students.add(h);
+    }
+
+    @RequestMapping(value = "/students", method = RequestMethod.POST )
     @ResponseBody
     public String getStudents(@RequestParam Integer classID) {
         return students.get(classID-1).toString();
+    }
+
+    @RequestMapping(value = "/students", method = RequestMethod.DELETE )
+    @ResponseBody
+    public void delStudents(@RequestParam Integer classID) {
+        students.remove(classID-1);
+        System.out.println(students);
     }
 
 
